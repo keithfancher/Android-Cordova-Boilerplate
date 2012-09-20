d9144733f0240b2153d09cf02283ebebf840ccd3
ktf <not.discostoo@gmail.com>
2012-09-20 15:03:21 -0700

    initial cordova setup

--- src/org/asdf/primer/PrimerActivity.java
+++ src/org/asdf/primer/PrimerActivity.java
@@ -1,20 +1,13 @@
 package org.asdf.primer;
 
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import org.apache.cordova;
 
-public class PrimerActivity extends Activity {
+public class PrimerActivity extends DroidGap {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.layout_primer);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.layout_primer, menu);
-        return true;
+        super.loadUrl("file:///android_asset/www/index.html");
     }
 }
